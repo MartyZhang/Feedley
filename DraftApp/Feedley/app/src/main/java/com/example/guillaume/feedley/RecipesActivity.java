609d6756b1d8c1e3475e63060d4d58fdcb3d5dbe
chloepouprom <Chloe@nat-oitwireless-inside-vapornet100-a-19528.princeton.edu>
2014-11-15 15:38:57 -0500

    Next page in app

---
+++ DraftApp/Feedley/app/src/main/java/com/example/guillaume/feedley/RecipesActivity.java
@@ -0,0 +1,69 @@
+package com.example.guillaume.feedley;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
+import android.view.KeyEvent;
+import android.view.View.OnKeyListener;
+import android.widget.Toast;
+import android.widget.Button;
+import android.content.Intent;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class RecipesActivity extends Activity {
+
+    TextView tvView;
+    TextView tvView2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+
+        tvView = (TextView) findViewById(R.id.textView1);
+        tvView2 = (TextView) findViewById(R.id.textView2);
+        Intent intent = getIntent();
+
+    }
+}
+
+
