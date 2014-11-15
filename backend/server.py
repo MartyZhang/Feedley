dc22e691a5e75fa2891404f9f8ce2118e41b8c31
chloepouprom <Chloe@nat-oitwireless-inside-vapornet100-a-19528.princeton.edu>
2014-11-15 07:52:49 -0500

    modify return json
    Only return recipes from simplyrecipes and all recipes

--- backend/server.py
+++ backend/server.py
@@ -12,6 +12,7 @@ import config
 
 app = Flask(__name__)
 
+apiKEY = '0c25d304894f3e6d646227454280b8fe'
 
 def parse(data):
     properties = ['f2f_url', 'publisher', 'publisher_url', 'social_rank']
@@ -20,17 +21,26 @@ def parse(data):
             del i[j]
     return data
 
+
 # http://localhost:5000/getrecipes?items=chicken,tomato
 @app.route('/getrecipes', methods=['GET'])
 def get_recipes():
     response = request.args.get('items')
     items = str(response)
-    url = 'http://food2fork.com/api/search?key='+config.apiKEY+'&q='+items
+    url = 'http://food2fork.com/api/search?key='+apiKEY+'&q='+items
     json_data = json.load(urllib2.urlopen(url))
     formatted_data = parse(json_data)
     print url
-    return jsonify({'recipes': formatted_data})
+    d = formatted_data
+    recipes = {}
 
+    for recipe in formatted_data['recipes']:
+      if "simplyrecipe" in recipe['source_url']:
+        recipes[recipe['title']] = [recipe['image_url'], recipe['source_url']]
+      if "allrecipes" in recipe['source_url']:
+        recipes[recipe['title']] = [recipe['image_url'], recipe['source_url']]
+
+    return jsonify({'recipes': recipes})
 
 
 if __name__ == '__main__':
