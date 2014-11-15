a1c177a0aa46cf17f2053f9b9c940dfe4e8f197e
mattmili <matthewmilitante@gmail.com>
2014-11-15 04:43:15 -0500

    added property remover for json data

--- backend/server.py
+++ backend/server.py
@@ -6,10 +6,19 @@
 # DELETE: Delete info
 
 from flask import Flask, jsonify, request
-import urllib2, json
+import urllib2
+import json
 import config
 
 app = Flask(__name__)
+
+
+def parse(data):
+    properties = ['f2f_url', 'publisher', 'publisher_url', 'social_rank']
+    for i in data['recipes']:
+        for j in properties:
+            del i[j]
+    return data
 
 # http://localhost:5000/getrecipes?items=chicken,tomato
 @app.route('/getrecipes', methods=['GET'])
@@ -17,9 +26,12 @@ def get_recipes():
     response = request.args.get('items')
     items = str(response)
     url = 'http://food2fork.com/api/search?key='+config.apiKEY+'&q='+items
-    data = json.load(urllib2.urlopen(url))
+    json_data = json.load(urllib2.urlopen(url))
+    formatted_data = parse(json_data)
     print url
-    return jsonify({'recipes': data})
+    return jsonify({'recipes': formatted_data})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
