078831ee105b89e56d31d17382450c421d957692
chloepouprom <Chloe@nat-oitwireless-inside-vapornet100-a-19528.princeton.edu>
2014-11-15 10:47:43 -0500

    Fixed ingredients and recipe formatting

--- backend/server.py
+++ backend/server.py
@@ -27,41 +27,21 @@ def parse(data):
 def get_recipe_allrecipe(request, title):
 #  request = 'http://allrecipes.com/Recipe/Slow-Cooker-Chicken-Tortilla-Soup/Detail.aspx'
   req = request[title]['source_url'].replace('Detail.aspx', 'kitchenview.aspx') # Reformat to kitchenview.aspx
-  print req
   page = requests.get(req)
 
   tree = html.fromstring(page.text)
   ingredients = tree.xpath('//ul[@class="generaltext"]/li/text()')
+  ingredients_formatted = []
   recipe = tree.xpath('//div[@class="direction"]/text()')
+  recipe_formatted = []
 
-  print 'INGREDIENTS'
   for ingredient in ingredients:
-    print ingredient.replace('\r\n', '', 2)
+    ingredients_formatted.append(ingredient.strip())
 
-  print 'RECIPE'
   for instruction in recipe:
-    print instruction.replace('\r\n', '')
+    recipe_formatted.append(instruction.strip())
 
-  return {'ingredients': ingredients, 'recipe': recipe}
-
-
-## Get recipe from simplyrecipes.com
-def get_recipe_simplyrecipes(request, title):
-  page = requests.get(request[title]['source_url'])
-  tree = html.fromstring(page.text)
-  ingredients = tree.xpath('//li[@class="ingredient"]/text()')
-  recipe = tree.xpath('//div[@itemprop="recipeInstructions"]/p/text()')
-
-  print 'INGREDIENTS'
-  for ingredient in ingredients:
-    print ingredient
-
-  print 'RECIPE'
-  for instruction in recipe:
-    print instruction
-
-  return {'ingredients': ingredients, 'recipe': recipe}
-
+  return {'ingredients': ingredients_formatted, 'recipe': recipe_formatted}
 
 # http://localhost:5000/getrecipes?items=chicken,tomato
 @app.route('/getrecipes', methods=['GET'])
@@ -72,22 +52,15 @@ def get_recipes():
 
     json_data = json.load(urllib2.urlopen(url))
     formatted_data = parse(json_data)
-    print url
     d = formatted_data
     recipes = {}
 
     for recipe in formatted_data['recipes']:
-      if "simplyrecipe" in recipe['source_url']:
-        image_and_source = {recipe['title']: {'image_url': recipe['image_url'], 'source_url': recipe['source_url']}}
-        recipes[recipe['title']] = {'image_url': image_and_source[recipe['title']]['image_url'], 'source_url': image_and_source[recipe['title']]['source_url'], 'ingredients': get_recipe_simplyrecipes(image_and_source, image_and_source.keys()[0])['ingredients'], 'recipe': get_recipe_simplyrecipes(image_and_source, image_and_source.keys()[0])['recipe'] }
-
       if "allrecipes" in recipe['source_url']:
         image_and_source = {recipe['title']: {'image_url': recipe['image_url'], 'source_url': recipe['source_url']}}
         recipes[recipe['title']] = {'image_url': image_and_source[recipe['title']]['image_url'], 'source_url': image_and_source[recipe['title']]['source_url'], 'ingredients': get_recipe_allrecipe(image_and_source, image_and_source.keys()[0])['ingredients'], 'recipe': get_recipe_allrecipe(image_and_source, image_and_source.keys()[0])['recipe'] }
 
     return jsonify({'recipes': recipes})
-
-
 
 
 if __name__ == '__main__':
